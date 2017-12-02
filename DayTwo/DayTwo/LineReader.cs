7900a76d378c1c6643de46808b5513999b0ca42b
Robert <robertleelivingston@gmail.com>
2017-12-02 14:49:50 -0600

    Finish day two

---
+++ DayTwo/DayTwo/LineReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DayTwo
+{
+    public class LineReader
+    {
+        public IEnumerable<string> ReadLine(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                string line;
+                while (null != (line = reader.ReadLine()))
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
---
+++ DayTwo/DayTwo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DayTwo
+{
+    public class Program
+    {
+        public static int sum = 0;
+        static void Main(string[] args)
+        {
+            // Part One
+
+            //foreach (var line in new LineReader().ReadLine("INPUT.txt"))
+            //{
+            //    var splitInput = line.Split(' ');
+            //    var numbers = splitInput.ToArray().Select(s => int.Parse(s.ToString()));
+            //    sum += numbers.Max() - numbers.Min();
+            //}
+
+            // Part Two
+
+            foreach (var line in new LineReader().ReadLine("INPUT.txt"))
+            {
+                var splitInput = line.Split(' ');
+                var numbers = splitInput.ToArray().Select(s => int.Parse(s.ToString()));
+
+                FindDivisibleNumbers(numbers);
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+        private static void FindDivisibleNumbers(IEnumerable<int> numbers)
+        {
+            foreach (var number in numbers)
+            {
+                var goodNumbers = numbers.Where(x => x % number == 0 && x != number).Select(x => x / number);
+
+                if (goodNumbers.Count() != 0)
+                {
+                    sum += goodNumbers.First();
+                }
+            }
+        }
+    }
+}
