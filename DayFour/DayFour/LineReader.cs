38cd1924af12155d6a0948e0ca30e1de2241a489
Robert <robertleelivingston@gmail.com>
2017-12-03 23:35:51 -0600

    Day Four Solution

---
+++ DayFour/DayFour/LineReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DayFour
+{
+    public class LineReader
+    {
+        public IEnumerable<string> ReadLine(string input)
+        {
+            using (var reader = new StreamReader(input))
+            {
+                string line;
+                while (null != (line = reader.ReadLine()))
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
---
+++ DayFour/DayFour/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace DayFour
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var count = 0;
+            var duplicate = false;
+            foreach (var line in new LineReader().ReadLine("INPUT.txt"))
+            {
+                var words = line.Split(' ');
+
+                // Part One
+
+                //for (var i = 0; i < words.Length; i++)
+                //{
+                //    for (var j = i + 1; j < words.Length; j++)
+                //    {
+                //        if (words[i].Equals(words[j]))
+                //        {
+                //            duplicate = true;
+                //        }
+                //    }
+                //}
+
+                // Part Two
+
+                for (var i = 0; i < words.Length; i++)
+                {
+                    for (var j = i + 1; j < words.Length; j++)
+                    {
+                        if (string.Concat(words[i].OrderBy(c => c)).Equals(string.Concat(words[j].OrderBy(c => c))))
+                        {
+                            duplicate = true;
+                        }
+                    }
+                }
+
+                if (duplicate == false)
+                {
+                    count++;
+                }
+                else
+                {
+                    duplicate = false;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
